e4978127c7f17be02353981daff11a16cef1bd92
Dimitris Asvestis <dimitris.asvestis@gmail.com>
2021-11-01 09:23:18 +0100

    reducers & thunk

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,3 +1,20 @@
+import {loadTodosFailure, loadTodosInProgress, loadTodosSuccess} from "./actions";
+
 export const displayAlert = text => () => {
-    alert(`You clicked on: ${text}`);
+    // alert(`You clicked on: ${text}`);
+    alert(text);
 };
+
+export const loadTodos = () => async (dispatch, getState) => {
+
+    try {
+        dispatch(loadTodosInProgress());
+        const response = await fetch('http://localhost:8080/todos');
+        const todos = await response.json();
+
+        dispatch(loadTodosSuccess(todos));
+    } catch (err) {
+        dispatch(loadTodosFailure());
+        dispatch(displayAlert(err));
+    }
+}
